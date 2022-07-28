ba10c606f9cf9fbfba7c8d8baf0ded4d2d661317
probe2k <kyash054@gmail.com>
2022-07-28 10:06:48 +0530

    wallpaper manager add

--- Dialer/MainActivity.java
+++ Dialer/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,29 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        phone = (EditText) findViewById(R.id.phone);
+        phone = findViewById(R.id.phone);
         save = findViewById(R.id.save);
         call = findViewById(R.id.call);
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String n = phone.getText().toString();
-                Intent i = new Intent(Intent.ACTION_INSERT);
-                i.setType(ContactsContract.Contacts.CONTENT_TYPE);
-                i.putExtra(ContactsContract.Intents.Insert.PHONE, n);
-                startActivity(i);
-            }
+        save.setOnClickListener(v -> {
+            String n = phone.getText().toString();
+            Intent i = new Intent(Intent.ACTION_INSERT);
+            i.setType(ContactsContract.Contacts.CONTENT_TYPE);
+            i.putExtra(ContactsContract.Intents.Insert.PHONE, n);
+            startActivity(i);
         });
 
-        call.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String n = phone.getText().toString();
-                Intent i = new Intent(Intent.ACTION_DIAL);
-                i.setData(Uri.parse("tel:" + n));
-                startActivity(i);
-            }
+        call.setOnClickListener(v -> {
+            String n = phone.getText().toString();
+            Intent i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:" + n));
+            startActivity(i);
         });
     }
 }
